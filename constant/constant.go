85b49a27cb2381e5e1b1acd1caa7a494efcf9774
Wabri <12409541+Wabri@users.noreply.github.com>
2024-11-20 20:07:27 +0100

    refactor(main): clean up the main function

---
+++ constant/constant.go
@@ -0,0 +1,18 @@
+package constant
+
+import "github.com/urfave/cli/v2"
+
+const Version = "dev"
+const Name = "harvester"
+const Description = "Harvester CLI to easily manage infrastructure"
+
+var Authors = []*cli.Author{
+	{
+		Name:  "Mohamed Belgaied Hassine",
+		Email: "mohamed.belgaiedhassine@gmail.com",
+	},
+	{
+		Name:  "Gabriele Puliti",
+		Email: "gabriele.puliti@proton.me",
+	},
+}
